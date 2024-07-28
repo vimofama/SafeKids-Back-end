225d3ab6738bff723c5f3236b83a8e9404160b19
CesarTufino <cesar.tufino@epn.edu.ec>
2024-07-27 22:51:45 -0500

    fix: delete logs endpoint

--- src/action-logs/action-logs.module.ts
+++ src/action-logs/action-logs.module.ts
@@ -1,12 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ActionLogsService } from './action-logs.service';
-import { ActionLogsController } from './action-logs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActionLog } from './entities/action-log.entity';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  controllers: [ActionLogsController],
   providers: [ActionLogsService],
   exports: [ActionLogsService],
   imports: [
